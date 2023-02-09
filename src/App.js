401d8e52ae8aaf2e5d54c17c8b6ff270b422b077
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-09 20:27:18 +0000

    The useState() Hook with Objects and Arrays part 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import ControlledFormHooks from "./components/ControlledFormHooks";
+import UseStateWithArrys from "./components/UseStateWithArrys";
+
 
 
 function App() {
   return (<div className="App">
-    <ControlledFormHooks />    
+    <UseStateWithArrys />    
   </div>);
 }
 
