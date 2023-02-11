83f742be0db69bcb3830d34f7eb5f78db3fb2ffc
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-11 22:46:26 +0000

    Fetching Data with GET

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
+import HTTPRequests from "./components/HTTPRequests";
+
 
 
 function App() {
   return (<div className="App">
-    <UseEffectCounterContainer />
+    <HTTPRequests />
   </div>);
 }
 
