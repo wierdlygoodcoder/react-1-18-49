4dde7a4f46998d856ae745fefa08e1c442c4fe00
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-12 05:05:52 +0000

    Sending Data with POST

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import HTTPRequests from "./components/HTTPRequests";
+import HTTPPost from "./components/HTTPPost";
 
 
 
 function App() {
   return (<div className="App">
-    <HTTPRequests />
+    <HTTPPost />
   </div>);
 }
 
